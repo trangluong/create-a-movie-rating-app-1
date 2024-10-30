b70044e31b00891ffb69a0172b1e0e3a408702bc
trang luong <luongngoctrang0105@gmail.com>
2024-10-30 23:17:06 +0700

    Added my project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { items } from "./movies.json";
+import MovieCard from "./MovieCard.vue";
+/*
+ This is an Icon that you can use to represent the stars if you like
+ otherwise you could just use a simple ⭐️ emoji, or * character.
+*/
+// import { StarIcon } from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+  <!-- This is where your template goes	-->
+  <div class="movies">
+    <template v-for="movie in items" :key="movie.id">
+      <MovieCard :movie="movie"></MovieCard>
+    </template>
+  </div>
+</template>
+<style lang="scss" scoped>
+.movies {
+  display: flex;
+  gap: 10px;
+}
+</style>
---
+++ src/MovieCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="movie" > 
+    <img class="movie-img" :src="movie.image"/>
+    <div class="movie-content ">
+      <div class="movie-title">{{ movie.name }}</div>
+      <div class="movie-genres" >
+        <div class="tag" v-for="gen in movie.genres" :key="gen"> {{ gen }}</div>
+      </div>
+      <div class="movie-desc">{{ movie.description }}</div>
+      <div class="movie-rating">
+        <span>{{ `Rating ${movie.rating}/5` }}</span>
+        <div class="stars">
+          <div v-for="s in movie.rating" :key="s" class="star">
+            ⭐️
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  movie: Object
+})
+</script>
+
+<style lang="scss" scoped>
+.movie {
+  width: 300px;
+  height: 550px;
+
+  .movie-img {
+    height: 60%;
+    width: 100%;
+  }
+  .movie-content {
+    flex-grow: 1;
+    padding: 10px 15px;
+    background-color: white;
+    .movie-genres {
+      display: flex;
+      flex-direction: row;
+      gap: 10px;
+    }
+    .tag {
+      padding: 2px;
+      background-color: blueviolet;
+      border-radius: 7px;
+    }
+    .movie-rating {
+      display: flex;
+      gap: 5px;
+    }
+    .stars {
+      display: flex;
+      gap: 5px;
+    }
+  }
+}
+</style>
